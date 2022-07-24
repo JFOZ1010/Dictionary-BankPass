98bddb34d4b27fe3888bba897724013b626f2b72
Juan Felipe Osorio <82562585+JFOZ1010@users.noreply.github.com>
2022-07-24 00:03:05 -0500

    Update cupp.py

--- cupp.py
+++ cupp.py
@@ -4,30 +4,6 @@
 #
 #  CUPP
 #  Common User Passwords Profiler
-#
-#  [Author]
-#
-#  Muris Kurgas aka j0rgan
-#  j0rgan [at] remote-exploit [dot] org
-#  http://www.remote-exploit.org
-#  http://www.azuzi.me
-#
-#  [License]
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
 
 
 import argparse
